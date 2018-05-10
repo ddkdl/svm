d2302962f082bacf543248b59738dfa450c2bbeb
Alvaro Esperanca <alvaro.sdb.esperanca@gmail.com>
2018-05-10 13:40:18 -0400

    Updated stopword map from string to boolean

--- preprocessor/tweetTokenizer.go
+++ preprocessor/tweetTokenizer.go
@@ -1,5 +1,13 @@
 package preprocessor
 
+import "strings"
+
 func Tokenize(document string) {
-	
+	tokens := strings.Fields(document)
+
+	for _, element := range tokens {
+		if stopwords[element] == true {
+			continue
+		}
+	}
 }
