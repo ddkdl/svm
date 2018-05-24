966bbda2439e2f55f77d7d0591b4f381a23adb24
Alvaro Esperanca <alvaro.sdb.esperanca@gmail.com>
2018-05-24 15:17:10 -0400

    Methods for creating gram matrix, and D matrix for QP solver complete

--- driver.go
+++ driver.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 
+	"gonum.org/v1/gonum/mat"
+
+	"github.com/ddkdl/svm/model"
 	"github.com/ddkdl/svm/preprocessor"
 )
 
@@ -15,5 +18,30 @@ func main() {
 		{"arroz", "tomate", "tomate"},
 	}
 
-	fmt.Println(preprocessor.CreateDTM(tweets))
+	X := preprocessor.CreateDTM(tweets)
+	fmt.Println("Printing X")
+	printMatrix(X)
+
+	K := model.CalculateGramMatrix(X)
+	fmt.Println("Printing K")
+	printMatrix(K)
+
+	y := mat.NewVecDense(5, []float64{1, 1, 0, 0, 1})
+
+	yOuter := model.CalculateYOuterProduct(y)
+	fmt.Println("Printing yOuter")
+	printMatrix(yOuter)
+
+	res := model.CalculateD(K, yOuter)
+	fmt.Println("Printing D")
+	printMatrix(res)
+
+}
+
+func printMatrix(a *mat.Dense) {
+	rows, _ := a.Dims()
+
+	for i := 0; i < rows; i++ {
+		fmt.Println(a.RowView(i))
+	}
 }
